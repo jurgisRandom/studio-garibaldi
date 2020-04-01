eee84e28e2a0e429f3d11df46bcd1705edd48b9c
Jurgis <jurgis.lietunovas@random.studio>
2020-04-01 20:55:07 +0200

    remove unneeded translation

--- config/i18n.js
+++ config/i18n.js
@@ -11,15 +11,6 @@ module.exports = {
         defaultTitle: `Gatsby Starter with multi-language and CMS`,
         defaultDescription: `Gatsby example site using Markdown, i18n and CMS`,
     },
-    pt: {
-        path: `pt`,
-        locale: `pt-BR`,
-        dateFormat: `DD/MM/YYYY`,
-        siteLanguage: `pt`,
-        ogLanguage: `pt_BR`,
-        defaultTitle: `Gatsby Starter multilingue com CMS`,
-        defaultDescription: `Exemplo de Gatsby site usando Markdown, i18n e CMS`,
-    },
     nl: {
         path: `nl`,
         locale: `nl-NL`,
